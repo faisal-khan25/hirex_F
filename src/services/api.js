8af2e982e4c3b5d6b5293ed9b4bf39fe23b4cf08
Faisal khan <pathanfaisalkhan313@gmail.com>
2026-07-09 11:23:37 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const BACKEND_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:8080";
+  import.meta.env.VITE_API_URL || "https://hirex-b.onrender.com";
 
 const api = axios.create({
   baseURL: BACKEND_URL,
